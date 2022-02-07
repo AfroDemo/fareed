fc7a90ac4492c29a7e3989c9b7e076f410938da1
AfroDemo <hasaniymkindi@gmail.com>
2022-02-07 11:21:47 +0300

    kwanza

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import react from 'react';
+import './index.css'
+import Navbar from './components/Navbar';
+import About from './components/About';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
+import Testimonials from './components/Testimonials';
+import Contacts from './components/Contact';
+import Footer from "./components/footer";
+
+function App() {
+  return (
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar/>
+      <About/>
+      <Projects/>
+      <Contacts/>
+      <Footer/>
+      {/* <About/>
+      <Projects/>
+      <Skills/>
+      <Testimonials/>
+      <Contacts/> */}
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Route, Routes, Link, Navigate, useNavigate } from "react-router-dom";
+
+function Footer() {
+        return (
+            <footer className="w-1/2 mx-auto h-8 transparent bg-gray-600 bottom-0 items-center inset-x-0">
+                <div className="flex justify-center m-3 pt-1 w-100">
+                   &copy; <a href="https://www.afrodemoz.one"> ADZ Community</a>
+                </div>
+            </footer>
+        )
+}
+
+export default Footer;
