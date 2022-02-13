8e94bb9bf9843768f9b4e2f643e30a6432e10677
AfroDemo <hasaniymkindi@gmail.com>
2022-02-13 12:51:04 +0300

    data

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,6 @@
 import { CodeIcon } from "@heroicons/react/solid";
 import React from "react";
-import { projects } from "../data";
+import ndepo from "../asset/ndepo.jpeg";
 
 function Projects() {
     return (
@@ -16,29 +16,27 @@ function Projects() {
             </p>
           </div>
           <div className="flex flex-wrap -m-4">
-            {projects.map((project) => (
               <a
-                href={project.link}
-                key={project.image}
+                href="afrodemoz.one"
+                key="tembo"
                 className="sm:w-1/2 w-100 p-4">
                 <div className="flex relative">
                   <img
                     alt="gallery"
                     className="absolute inset-0 w-full h-full object-cover object-center"
-                    src={project.image}
+                    src={ndepo}
                   />
                   <div className="px-8 py-10 relative z-10 w-full border-4 border-gray-800 bg-gray-900 opacity-0 hover:opacity-100">
                     <h2 className="tracking-widest text-sm title-font font-medium text-green-400 mb-1">
-                      {project.subtitle}
+                    Cross Up View
                     </h2>
                     <h1 className="title-font text-lg font-medium text-white mb-3">
-                      {project.title}
+                    Mikumi National Park
                     </h1>
-                    <p className="leading-relaxed">{project.description}</p>
+                    <p className="leading-relaxed">I love nature also doing Wildlife Photography</p>
                   </div>
                 </div>
               </a>
-            ))}
           </div>
         </div>
       </section>
