244cf006d1b6ad61acb1b4b564c4602a54251037
Hasani Mkindi <36568909+AfroDemo@users.noreply.github.com>
2023-08-07 10:34:16 +0300

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,7 @@ function Footer() {
         return (
             <footer className="w-1/2 mx-auto h-8 transparent bg-gray-600 bottom-0 items-center inset-x-0">
                 <div className="flex justify-center m-3 pt-1 w-100">
-                   &copy; <a href="https://www.afrodemoz.one"> ADZ Community</a>
+                   &copy; <a href="https://afrodemoz.tk/"> ADZ Community</a>
                 </div>
             </footer>
         )
